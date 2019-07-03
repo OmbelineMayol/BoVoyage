088d1fea12d211d3aaa2bdcc4accf09c5610fad9
OmbelineMayol <ombeline.mayol@gmail.com>
2019-07-03 15:07:35 +0200

    MAJ

--- BoVoyage/src/main/java/com/intiformation/bovoyage/entity/Accompagnant.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/entity/Accompagnant.java
@@ -1,12 +1,17 @@
 package com.intiformation.bovoyage.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Accompagnant extends Personne{
 	
+	/* -------- ASSOCIATIONS UML -----------------*/
 	
-	
+	@ManyToOne
+	@JoinColumn(name="client_id", referencedColumnName="id_client")
+	private Client client;
 	
 
 }
