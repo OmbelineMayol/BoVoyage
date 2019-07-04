25c7b40ebdd606bf1c306cde041c43614efc2222
OmbelineMayol <ombeline.mayol@gmail.com>
2019-07-04 15:34:54 +0200

    WS client

--- BoVoyage/src/main/java/com/intiformation/bovoyage/wsrest/ClientWSRest.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/wsrest/ClientWSRest.java
@@ -7,37 +7,47 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.intiformation.bovoyage.entity.Client;
 import com.intiformation.bovoyage.service.IClientService;
 
 
-
+@RestController
 public class ClientWSRest {
 	
-	/**
-	 * Liaison à la couche Service
-	 */
+	/*--------- ASSOCIATION AVEC LA COUCHE SERVICE -------------- */
+	
 	@Autowired
 	IClientService clientService;
 
+	// Setter pour l'injection Spring
+	
+	public void setClientService(IClientService clientService) {
+		this.clientService = clientService;
+	}
+	
+
+	/*------------------------- METHODE ------------------------ */
+		
 	/**
 	 * Liste des clients
 	 * @return
 	 */
-	@RequestMapping(value = "/clients", method = RequestMethod.GET, headers = "Accept=application/json")
+	@RequestMapping(value = "/clients/getAll", method = RequestMethod.GET, headers = "Accept=application/json")
 	public List<Client> getAllClients() {
 		List<Client> listeClients = clientService.getAllClients();
 		return listeClients;
 	}
+
 
 	/**
 	 * Selection d'un client
 	 * @param idClientIn
 	 * @return
 	 */
-	@RequestMapping(value = "/clients/{idClientIn}", method = RequestMethod.GET, produces={"application/json"} )
-	public Client getClientById(@PathVariable int idClientIn) {
+	@RequestMapping(value = "/clients/getById/{idClient}", method = RequestMethod.GET, produces={"application/json"} )
+	public Client getClientById(@PathVariable("idClient") int idClientIn) {
 		return clientService.getByIdClientService(idClientIn);
 	}
 
@@ -45,7 +55,7 @@ public class ClientWSRest {
 	 * Ajout d'un client
 	 * @param clientIn
 	 */
-	@RequestMapping(value = "/clients", method = RequestMethod.POST, produces= "application/json", consumes="application/json")
+	@RequestMapping(value = "/clients/add", method = RequestMethod.POST, produces= "application/json", consumes="application/json")
 	public void addClient(@RequestBody Client clientIn) {
 		clientService.addClientService(clientIn);
 	}
@@ -54,7 +64,7 @@ public class ClientWSRest {
 	 * Modification d'un client
 	 * @param clientIn
 	 */
-	@RequestMapping(value = "/clients", method = RequestMethod.PUT, produces= "application/json",consumes="application/json")
+	@RequestMapping(value = "/clients/update", method = RequestMethod.PUT, produces= "application/json",consumes="application/json")
 	public void updateClient(@RequestBody Client clientIn) {
 		clientService.updateClientService(clientIn);
 
@@ -64,8 +74,8 @@ public class ClientWSRest {
 	 * Suppression d'un client
 	 * @param idClientIn
 	 */
-	@RequestMapping(value = "/clients/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
-	public void deleteClient(@PathVariable("id") int idClientIn) {
+	@RequestMapping(value = "/clients/delete/{idClient}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+	public void deleteClient(@PathVariable("idClient") int idClientIn) {
 		clientService.deleteClientService(idClientIn);
 
 	}
