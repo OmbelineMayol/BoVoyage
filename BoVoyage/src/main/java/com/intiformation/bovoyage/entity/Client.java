ab63440ba4ddaf633d3eb888fe52542fcea13e93
OmbelineMayol <ombeline.mayol@gmail.com>
2019-07-04 10:23:27 +0200

    Correction de la DAO agent
    Modification des entity

--- BoVoyage/src/main/java/com/intiformation/bovoyage/entity/Client.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/entity/Client.java
@@ -13,15 +13,38 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+
+
 @Entity(name= "client")
 @Table(name= "clients")
-public class Client extends Personne{
+public class Client {
 
 	// Déclaration des attributs
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_client")
-	private String idClient;
+	protected int idClient;
+	
+	@Column(name = "civilite")
+	protected int civilite;
+	
+	@Column(name = "nom")
+	protected String nom;
+	
+	@Column(name = "prenom")
+	protected String prenom;
+	
+	@Column(name = "age")
+	protected int age;
+	
+	@Column(name = "adresse")
+	protected String adresse;
+	
+	@Column(name = "telephone")
+	protected String telephone;
+	
+	@Column(name = "email")
+	protected String email;	
 	
 	@Column(name = "password")
 	private String password;
@@ -32,7 +55,7 @@ public class Client extends Personne{
 	@Column(name = "actived")
 	private int actived;
 
-	//Transformation de l'association UML en java
+	// ----- Transformation de l'association UML en java
 	@OneToOne(mappedBy="client")
 	private Role role;
 	
@@ -41,27 +64,94 @@ public class Client extends Personne{
 	
 	@OneToOne(mappedBy="client")
 	private Reservation reservation;
-	
-	// Déclaration des constructeurs
+
+	// ----- Déclaration des constructeurs
 	public Client() {
 		super();
 	}
 
-	public Client(String password, double numCB, int actived) {
+	public Client(int civilite, String nom, String prenom, int age, String adresse, String telephone, String email,
+			String password, double numCB, int actived, Role role, List<Accompagnant> listeAccompagnants,
+			Reservation reservation) {
 		super();
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		this.adresse = adresse;
+		this.telephone = telephone;
+		this.email = email;
 		this.password = password;
 		this.numCB = numCB;
 		this.actived = actived;
+		this.role = role;
+		this.listeAccompagnants = listeAccompagnants;
+		this.reservation = reservation;
 	}
+	// ----- Déclaration des getters et setters
 
-
-	// Déclaration getters et setters
-	public String getIdClient() {
+	public int getIdClient() {
 		return idClient;
 	}
 
-	public void setIdClient(String idClient) {
+	public void setIdClient(int idClient) {
 		this.idClient = idClient;
+	}
+
+	public int getCivilite() {
+		return civilite;
+	}
+
+	public void setCivilite(int civilite) {
+		this.civilite = civilite;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPassword() {
@@ -111,6 +201,10 @@ public class Client extends Personne{
 	public void setReservation(Reservation reservation) {
 		this.reservation = reservation;
 	}
+	
+	
+	
+	
 	
 
 }
