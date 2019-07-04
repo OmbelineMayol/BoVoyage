082996a337d6636e7554cadc6e0776396163da51
IN-MP-037 <IN-MP-037@192.168.1.56>
2019-07-04 11:16:56 +0200

    role dao et s

--- BoVoyage/src/main/java/com/intiformation/bovoyage/service/IRoleService.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/service/IRoleService.java
@@ -1,5 +1,18 @@
 package com.intiformation.bovoyage.service;
 
-public interface IRoleService {
+import java.util.List;
 
+import com.intiformation.bovoyage.entity.Role;
+
+public interface IRoleService {
+	
+public void addRoleDao (Role pRole);
+	
+	public Role getByIdRoleDao (int pIdRole);
+	
+	public void updateRoleDao (Role pRole);
+	
+	public void deleteRoleDao (int pIdRole);
+	
+	public List<Role> getAllRoles();
 }
