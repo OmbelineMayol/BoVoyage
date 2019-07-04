5978d5a09f33ee041b8f12fa107cbed0827151d3
OmbelineMayol <ombeline.mayol@gmail.com>
2019-07-04 12:55:22 +0200

    Dao Formuel

--- BoVoyage/src/main/java/com/intiformation/bovoyage/dao/FormuleDaoImpl.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/dao/FormuleDaoImpl.java
@@ -1,5 +1,89 @@
 package com.intiformation.bovoyage.dao;
 
-public class FormuleDaoImpl {
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.intiformation.bovoyage.entity.Formule;
+import com.intiformation.bovoyage.entity.Hotel;
+
+@Repository
+public class FormuleDaoImpl implements IFormuleDao {
+
+	/* --------------- ASSOCATIOND UML EN JAVA --------------------- */
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// Setter pour l'injection Srping
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	/* --------------- REDEFINITION DES METHODE --------------------- */
+
+	@Transactional(readOnly = true)
+	public List<Formule> getAllFormuleDao() {
+		// TODO Auto-generated method stub
+		return sessionFactory.getCurrentSession().createQuery("FROM formule").list();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Formule> getFormuleByPaysDao(String nomPays) {
+		// 1. Recuperation de la session
+		Session session = sessionFactory.getCurrentSession();
+
+		// 2. Creation de la requeteHQL
+
+		String reqHQL = "SELECT f FROM formule f WHERE f.pays=?1";
+
+		// 3. Creation de la requete
+		Query query = session.createQuery(reqHQL);
+
+		// 4. Passage des paramètres dans la requete
+
+		query.setParameter(1, nomPays);
+
+		// 5. Envoi de la requete et recuperation du resultat
+
+		List<Formule> listOut = query.list();
+
+		return listOut;
+	}
+
+	@Transactional(readOnly = true)
+	public List<Formule> getFormuleByContinentDao(String nomContinent) {
+
+		// 1. Recuperation de la session
+		Session session = sessionFactory.getCurrentSession();
+
+		// 2. Creation de la requeteHQL
+
+		String reqHQL = "SELECT f FROM formule f WHERE f.continent=?1";
+
+		// 3. Creation de la requete
+		Query query = session.createQuery(reqHQL);
+
+		// 4. Passage des paramètres dans la requete
+
+		query.setParameter(1, nomContinent);
+
+		// 5. Envoi de la requete et recuperation du resultat
+
+		List<Formule> listOut = query.list();
+
+		return listOut;
+	}
+
+	@Transactional(readOnly = true)
+	public Formule getFormuleById(int idFormule) {
+
+		return (Formule) sessionFactory.getCurrentSession().get(Formule.class, idFormule);
+	}
 
 }
--- BoVoyage/src/main/java/com/intiformation/bovoyage/dao/IFormuleDao.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/dao/IFormuleDao.java
@@ -1,7 +1,17 @@
 package com.intiformation.bovoyage.dao;
 
+import java.util.List;
+
+import com.intiformation.bovoyage.entity.Formule;
+
 public interface IFormuleDao {
 	
+	public List<Formule> getAllFormuleDao();
 	
+	public List<Formule> getFormuleByPaysDao(String nomPays);
+	
+	public List<Formule> getFormuleByContinentDao(String nomContinent);
+	
+	public Formule getFormuleById(int idFormule);
 
 }
