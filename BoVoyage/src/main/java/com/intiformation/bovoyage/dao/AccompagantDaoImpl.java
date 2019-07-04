6bbac3906582d0573fdfea01e63d50b03bcdc2f1
IN-MP-043 <IN-MP-043@192.168.1.76>
2019-07-04 15:27:50 +0200

    update accompagnant dao flo

--- BoVoyage/src/main/java/com/intiformation/bovoyage/dao/AccompagantDaoImpl.java
+++ BoVoyage/src/main/java/com/intiformation/bovoyage/dao/AccompagantDaoImpl.java
@@ -2,11 +2,12 @@ package com.intiformation.bovoyage.dao;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
+
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.intiformation.bovoyage.entity.Accompagnant;
 
